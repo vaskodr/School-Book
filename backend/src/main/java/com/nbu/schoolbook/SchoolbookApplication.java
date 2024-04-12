8b087438af261e6446fe9aa5aba5ffb58f773cf5
Vasil Vasilev <vassko.vassilew@gmail.com>
2024-04-12 18:15:33 +0300

    Initial commit

---
+++ backend/src/main/java/com/nbu/schoolbook/SchoolbookApplication.java
@@ -0,0 +1,13 @@
+package com.nbu.schoolbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolbookApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nbu/schoolbook/SchoolbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbu.schoolbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
